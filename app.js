eeaa853feab2e97c0c96c4eaf7481c808a26157c
brenley-woodard <brenleyew@gmail.com>
2022-11-17 12:04:44 -0800

    list for dogs is displaying woot woot

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 /* Imports */
+import { fetchDogs } from './fetch-utils.js';
+import { renderDogs } from './render-utils.js';
 
 /* Get DOM Elements */
-
+const dogDiv = document.getElementById('dogs');
 /* State */
 
 /* Events */
+window.addEventListener('load', async () => {
+    const dogs = await fetchDogs();
+
+    for (let dog of dogs) {
+        const dogObject = renderDogs(dog);
+        dogDiv.append(dogObject);
+    }
+});
 
 /* Display Functions */
 
--- render-utils.js
+++ render-utils.js
@@ -1,1 +1,15 @@
-export function renderDogs() {}
+export function renderDogs(dog) {
+    const div = document.createElement('div');
+
+    const h3 = document.createElement('h3');
+    h3.textContent = dog.name;
+
+    const p = document.createElement('p');
+    p.textContent = dog.colors;
+
+    const p2 = document.createElement('p');
+    p2.textContent = dog.characteristics;
+
+    div.append(h3, p, p2);
+    return div;
+}
